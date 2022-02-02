ce4c21923671361cc78b8f58cf0fcdd2ccbf081e
Nick Janetakis <nick.janetakis@gmail.com>
2022-02-02 08:19:57 -0500

    Update docstring for Black

--- src/pages/tests.py
+++ src/pages/tests.py
@@ -3,11 +3,11 @@ from django.test import TestCase
 
 class ViewTests(TestCase):
     def test_home_page(self):
-        """ Home page should respond with a success 200. """
+        """Home page should respond with a success 200."""
         response = self.client.get("/", follow=True)
         self.assertEqual(response.status_code, 200)
 
     def test_up(self):
-        """ Up page should respond with a success 200. """
+        """Up page should respond with a success 200."""
         response = self.client.get("/up", follow=True)
         self.assertEqual(response.status_code, 200)
