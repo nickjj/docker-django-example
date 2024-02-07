3d1fe307fb4f24c044cbeb066f3ef95c9861055d
Nick Janetakis <nick.janetakis@gmail.com>
2024-02-07 07:32:16 -0500

    Format code for Black

--- src/config/asgi.py
+++ src/config/asgi.py
@@ -6,6 +6,7 @@ It exposes the ASGI callable as a module-level variable named `application`.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
+
 import os
 
 from django.core.asgi import get_asgi_application
