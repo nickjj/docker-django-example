099a5458cfde60a6ef968970eb4abb979cda7751
DTU-travelpals <daniel.tomsu@travelpals.de>
2025-09-04 13:27:46 +0200

    changed gitignore and add type to Celery

--- src/config/celery.py
+++ src/config/celery.py
@@ -4,6 +4,6 @@ from celery import Celery
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
-app = Celery("hello")
+app: Celery = Celery("hello")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
