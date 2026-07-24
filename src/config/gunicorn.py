6070eee640dd37c63b9b20a9e8586203bf04215d
Nick Janetakis <nick.janetakis@gmail.com>
2026-07-24 12:12:55 -0400

    Fix Ruff >= 0.16 issues

--- src/config/gunicorn.py
+++ src/config/gunicorn.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 import multiprocessing
 import os
 
@@ -8,12 +6,12 @@ from distutils.util import strtobool
 bind = f"0.0.0.0:{os.getenv('PORT', '8000')}"
 accesslog = "-"
 access_log_format = (
-    "%(h)s %(l)s %(u)s %(t)s '%(r)s' %(s)s %(b)s '%(f)s' '%(a)s' in %(D)sµs"  # noqa: E501
+    "%(h)s %(l)s %(u)s %(t)s '%(r)s' %(s)s %(b)s '%(f)s' '%(a)s' in %(D)sµs"
 )
 
 workers = int(os.getenv("WEB_CONCURRENCY", multiprocessing.cpu_count() * 2))
-threads = int(os.getenv("PYTHON_MAX_THREADS", 1))
+threads = int(os.getenv("PYTHON_MAX_THREADS", "1"))
 
 reload = bool(strtobool(os.getenv("WEB_RELOAD", "false")))
 
-timeout = int(os.getenv("WEB_TIMEOUT", 120))
+timeout = int(os.getenv("WEB_TIMEOUT", "120"))
